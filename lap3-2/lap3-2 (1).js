b96be4655faeea6d8d237532046ff516fc5705c5
“warat <watat.wr@mail.kmutt.ac.th>
2021-02-13 23:17:12 +0700

    lap3

---
+++ lap3-2/lap3-2 (1).js
@@ -0,0 +1,22 @@
+var image = document.querySelectorAll('img');
+for (let index = 0; index < image.length; index++) {
+    image[index].addEventListener('click',xxx);
+}
+
+function xxx(){
+this.src = 'https://e7.pngegg.com/pngimages/628/1018/png-clipart-x-mark-symbol-computer-icons-w-miscellaneous-cross.png';
+this.className = "x";
+
+
+
+do{
+    var o = Math.floor((Math.random() * 9));
+}while(image[o].className !== "free");
+
+
+image[ran].src = 'https://img2.thaipng.com/20180428/xje/kisspng-letter-case-o-alphabet-all-caps-dimensional-characters-26-english-letters-5ae471e0d98030.1299930115249208008909.jpg';
+this.className = "o";  
+
+}
+
+   
---
+++ lap3/lap3js1.js
@@ -0,0 +1,4 @@
+caharcator = prompt('input your favorite caharactor')
+console.log(caharcator+' is your favorite caharactor');
+colorc = prompt('input your caharactor color')
+console.log(colorc + ' is your favorite caharactor color');
